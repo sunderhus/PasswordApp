ec18ceb945515eead25a599591d50139bef8299a
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2019-11-27 14:22:09 -0300

    adicionado o cors ao projeto.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 //rota de redirecionamento para o index.
 app.use(express.static(path.join(__dirname, "public")));
 
 //porta da aplicação
-app.listen(PORT, function () {
-    console.log(`Aplicação rodando na porta: ${PORT}!`);
+app.listen(PORT, function() {
+  console.log(`Aplicação rodando na porta: ${PORT}!`);
 });
